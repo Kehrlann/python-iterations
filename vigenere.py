3ac227e34d1a1b82c45924630db68ca402302e7a
Daniel Garnier-Moiroux <git@garnier.wf>
2020-10-31 22:24:28 +0100

    add cesar (in vigenere file)

---
+++ vigenere.py
@@ -0,0 +1,16 @@
+def cesar(clear, key, encode=True):
+    """
+    retourne l'encryption du caractere <clear> par la clé <key>
+
+    le caractère <key> doit être un caractère alphabétique ASCII
+    c'est à dire que son ord() est entre ceux de 'a' et 'z' ou
+    entre ceux de 'A' et 'Z'
+
+    Parameters:
+      clear: le caractère à encoder
+      key: la clef d'encodage
+      encode: True pour encoder, False pour décoder
+    Returns:
+      le caractère encodé/décodé
+    """
+    return None
