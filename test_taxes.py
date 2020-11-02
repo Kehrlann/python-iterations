b0b8387f6cd8fc47ee3fbddc12cb2ffa3cbe5dca
Daniel Garnier-Moiroux <git@garnier.wf>
2020-11-02 21:50:56 +0100

    test taxes, better reporting for students

--- test_taxes.py
+++ test_taxes.py
@@ -1,24 +1,34 @@
 import unittest
 from taxes import taxes
 
+
 class TestTaxes(unittest.TestCase):
 
     def test_taxes(self):
         expected = [
-                (0, 0),
-                (50000, 7500),
-                (12500, 0),
-                (5000, 0),
-                (16500, 800),
-                (30000, 3500),
-                (100000, 27500),
-                (150000, 47500),
-                (200000, 70000),
-                (12504, 0)
-            ]
+            (0, 0),
+            (50000, 7500),
+            (12500, 0),
+            (5000, 0),
+            (16500, 800),
+            (30000, 3500),
+            (100000, 27500),
+            (150000, 47500),
+            (200000, 70000),
+            (12504, 0)
+        ]
+        errors = 0
         for income, tax in expected:
-            errmsg = f"for income {income}, taxes should be {tax}"
-            self.assertEqual(taxes(income), tax, errmsg)
+            if taxes(income) == tax:
+                print(f"OK    : income {income} -> tax {tax}")
+            else:
+                print(
+                    f"ERROR : income {income} -> should be {tax}, but was {taxes(income)}")
+                errors += 1
+
+        if errors:
+            self.fail(f"{errors} errors / {len(expected)} total")
+
 
 if __name__ == '__main__':
     unittest.main()
