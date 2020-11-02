b3a25df5a05e8609a17d9e862b3b10f3b499f0e8
Daniel Garnier-Moiroux <git@garnier.wf>
2020-11-02 22:20:43 +0100

    Add digits

---
+++ digits.py
@@ -0,0 +1,14 @@
+from itertools import count
+
+
+def dig_pow(n, p):
+    """
+    Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k ?
+
+    Parameters:
+        n: the base integer
+        p: the starting point of powers
+    Returns:
+        k if such integer exists, -1 otherwise
+    """
+    return None
