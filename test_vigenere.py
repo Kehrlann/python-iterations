751c3b29287f0e4615fd9b0232fed82703013a57
Daniel Garnier-Moiroux <git@garnier.wf>
2020-11-04 13:40:28 +0100

    Vigenere: use ascii characters because ... Github

--- test_vigenere.py
+++ test_vigenere.py
@@ -11,7 +11,7 @@ class TestVigenere(BaseTestCase):
             ('fq pqxvmlh', 'CLE', False, 'ce message'),
             ('une charogne', 'baudelaire', True, 'woz htbaglpf'),
             ("Rappelez-vous l'objet", 'Charles', True, "Uiqhqqxc-wggx o'ptvjm"),
-            ('que nous vîmes', 'baudelaire', True, 'svz savb aînzw')
+            ('que nous vimes?', 'baudelaire', True, 'svz savb aknzw?')
         ]
         self.run_tests(test_cases, vigenere)
 
